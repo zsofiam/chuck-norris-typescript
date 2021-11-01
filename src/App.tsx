12c320711bd8fdb2b4b1554a45371efdafb318f1
Zsofia Matyasi <zsofia.matyasi@gmail.com>
2021-11-01 14:36:43 +0100

    [format] routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import './App.css';
-import Header from './Components/Header/Header';
-import Joke from './Components/Joke/Joke';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Joke from "./Components/Joke/Joke";
+// import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-<div className="container">
-    <Router>
-      
+    <div className="container">
+      <BrowserRouter>
         <Header />
-      
-        <Route exact path="/" />
-        <Route path="/jokes/category=:category" component={Joke} />
-      
-      </Router>
-</div>
-    
+        <Switch>
+          <Route exact path="/" />
+          <Route path="/jokes/category=:category" component={Joke} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
