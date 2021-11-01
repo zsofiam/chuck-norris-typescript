3094367d4595dff8b952e72e45340abca47cfe62
Zsofia Matyasi <zsofia.matyasi@gmail.com>
2021-11-01 11:42:48 +0100

    [add] Joke.tsx details

--- src/Components/Joke.tsx
+++ src/Components/Joke.tsx
@@ -1,8 +1,29 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
 export default function Joke() {
-  
+    
+    const { category } = useParams<{category?: string}>();
+    const [joke, setJoke] = useState({value: ""});
+
+    useEffect(() => {
+      const jokeUrl = `https://api.chucknorris.io/jokes/random?category=${category}`;
+      axios.get(jokeUrl).then((res) => setJoke(res.data));
+    }, [setJoke, category]);
+
     return (
       <div className="wrapper">
-        
+        {joke.value ? (
+
+          <h2 id="joke">
+            <q>{joke.value}</q>
+          </h2>
+        ) : (
+          <div id="loading">
+            <p>Loading...</p>
+          </div>
+        )}
       </div>
     );
   }
