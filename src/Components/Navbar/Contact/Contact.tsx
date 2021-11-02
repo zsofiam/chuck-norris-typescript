3dc37c3ad6666a18ceb5e4228f750a251d7ede3b
Zsofia Matyasi <zsofia.matyasi@gmail.com>
2021-11-02 15:27:53 +0100

    [add] components to Navbar links

---
+++ src/Components/Navbar/Contact/Contact.tsx
@@ -0,0 +1,15 @@
+export default function Contact() {
+  return (
+    <div className="wrapper">
+      <h3>A React application with TypeScript</h3>
+      <p>
+        <a
+          href="https://www.facebook.com/officialchucknorrispage"
+          target="blank"
+        >
+          Contact Chuck Norris
+        </a>
+      </p>
+    </div>
+  );
+}
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,29 +1,27 @@
 import { Link } from "react-router-dom";
 import CategoriesList from "../Categories/CategoriesList";
-import './Navbar.css';
+import "./Navbar.css";
 
 export default function Navbar() {
-
-    return (
-      <>
-        <div className="navbar">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="#news">News</Link>
-            </li>
-            <li>
-              <Link to="#contact">Contact</Link>
-            </li>
-            <li>
-              <Link to="#about">About</Link>
-            </li>
-          </ul>
-          <CategoriesList/>
-        </div>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <div className="navbar">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/news">News</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+        <CategoriesList />
+      </div>
+    </>
+  );
+}
