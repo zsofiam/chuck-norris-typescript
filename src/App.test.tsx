7fc661efbb07f8d6f6064c80f0c301455e1b3c8d
Zsofia Matyasi <zsofia.matyasi@gmail.com>
2021-11-03 17:29:54 +0100

    [WIP] add tests to App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,35 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import * as ReactDOM from "react-dom";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/About/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component tests", () => {
+  let container: HTMLDivElement;
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+    ReactDOM.render(<App />, container);
+  });
+  afterEach(() => {
+    document.body.removeChild(container);
+    container.remove();
+  });
+
+  test("renders About link", () => {
+    // render(<App />);
+    const linkElement = screen.getByText(/About/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("renders Contact link", () => {
+    // render(<App />);
+    const linkElement = screen.getByText(/Contact/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("renders News link", () => {
+    // render(<App />);
+    const linkElement = screen.getByText(/News/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
